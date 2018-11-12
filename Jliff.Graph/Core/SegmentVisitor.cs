75f5eb0bda818acb484604ff0e96c712cbb04aa3
Phil Ritchie <phil.ritchie@vistatec.com>
2018-11-12 17:58:07 +0000

    Example of modifying segments through visitor.

---
+++ Jliff.Graph/Core/SegmentVisitor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Jliff.Graph.Interfaces;
+using Localization.Jliff.Graph;
+
+namespace Jliff.Graph.Core
+{
+    public class SegmentVisitor : ICompositeVisitor
+    {
+        public List<Segment> Segments = new List<Segment>();
+
+        public void Visit(JlfNode node)
+        {
+            switch (node)
+            {
+                case Segment s:
+                    Segments.Add(s);
+                    break;
+            }
+        }
+    }
+}
--- Jliff.Tests/VisitorTests.cs
+++ Jliff.Tests/VisitorTests.cs
@@ -1,4 +1,6 @@
-﻿using Jliff.Graph.Serialization;
+﻿using System.Collections.Generic;
+using Jliff.Graph.Core;
+using Jliff.Graph.Serialization;
 using Localization.Jliff.Graph;
 using Localization.Jliff.Graph.Modules.ResourceData;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -44,6 +46,46 @@ namespace UnitTests
             TestVisitor testVisitor = new TestVisitor();
             jlb.Files[0].Accept(testVisitor);
             (jlb.Files[0].Subfiles[0] as Unit).Accept(testVisitor);
+        }
+
+        [TestMethod]
+        public void VisitSegments()
+        {
+            JliffDocument jlb = new JliffBuilder("en-US", "it-IT")
+                .AddFile(new XlfEventArgs("f1"))
+                .AddUnit(new XlfEventArgs("u1"))
+                .EndSubFiles()
+                .AddSegment(new XlfEventArgs("s1"))
+                .EndSubUnits()
+                .AddSource("Hello")
+                .AddSmElement("mrk1", true)
+                .AddSource("there")
+                .AddEmElement("", true)
+                .AddTarget("Buongiorno")
+                .MoreSubUnits()
+                .AddSegment(new XlfEventArgs("s2"))
+                .EndSubUnits()
+                .AddSource("Congratulations")
+                .MoreSubUnits()
+                .AddSegment(new XlfEventArgs("s3"))
+                .EndSubUnits()
+                .AddSource("Goodbye")
+                .AddTarget("Arrivederci")
+                .Build();
+
+            SegmentVisitor sv = new SegmentVisitor();
+            jlb.Files[0].Process(sv);
+            Assert.AreEqual<int>(3, sv.Segments.Count);
+
+            sv.Segments[2].Target = new List<IElement>
+            {
+                new TextElement("So long")
+            };
+
+            Unit u = jlb.Files[0].Subfiles[0] as Unit;
+            Segment s = u.Subunits[2] as Segment;
+            TextElement t = s.Target[0] as TextElement;
+            Assert.AreEqual<string>("So long", t.Text);
         }
     }
 }
